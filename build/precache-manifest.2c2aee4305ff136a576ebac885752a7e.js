822bc42ce167b68784e8318d2c2579339b4bf9c0
darwin_b <darwin.bollepalli@gmail.com>
2020-10-08 01:20:46 +0530

    Build Files

---
+++ build/precache-manifest.2c2aee4305ff136a576ebac885752a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a5ddff8b3537e1c38ade9518ae1ee7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8a07a82cd7865ea6dad4",
+    "url": "./static/css/main.30d5f39e.chunk.css"
+  },
+  {
+    "revision": "faa6c5a245bf6bacb36e",
+    "url": "./static/js/2.18ea62cb.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.18ea62cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a07a82cd7865ea6dad4",
+    "url": "./static/js/main.a4255d24.chunk.js"
+  },
+  {
+    "revision": "e5e4eb28be283f38a6c9",
+    "url": "./static/js/runtime-main.c6271f04.js"
+  }
+]);
